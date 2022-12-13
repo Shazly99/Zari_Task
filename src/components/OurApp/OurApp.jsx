283811e817f0fc66874174bf551e237840b5c438
Shazly99 <ahmed.y.elshazly@gmail.com>
2022-12-13 12:22:16 +0200

    commit

--- src/components/OurApp/OurApp.jsx
+++ src/components/OurApp/OurApp.jsx
@@ -8,24 +8,24 @@ function OurApp() {
   return (
     <>
       <div className="app__OurApp  container container-fluid mt-5" id='ourapp'>
-        
+
 
         <div className='app__OurApp-main'>
           <div className='app__OurApp-left'>
             <h1 className='app__OurApp-h1'>Our App</h1>
             <p className='app__OurApp-p'>A new application to facilitate the follow-up of the external work team of shipping representatives or external sales personnel, and to measure the production capacity of the individual and the working hours.<br />
               Zari Falcon enables you to view everything that happens with your employee outside the company during working hours through its various services.</p>
-    
+
             <div className="app__OurApp-btn">
               <button className='app__home-btn-get'> <Icons.Ios fontSize={20} /> Available on the App Store</button>
               <button className='app__home-btn-watch'>  <Icons.google fontSize={20} /> Get on the Google Play</button>
             </div>
           </div>
 
-          <div className='app__OurApp-right d-flex justify-content-center align-items-center mt-5'> 
-          <img src={Img.Ourapp} className='app__OurApp-img'  />
+          <div className='app__OurApp-right d-flex justify-content-end align-items-center mt-5'>
+            <img src={Img.Ourapp} className='app__OurApp-img' />
           </div>
-        </div> 
+        </div>
       </div>
 
     </>
